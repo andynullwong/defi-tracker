9581f819ebd32e944878c18e35a4e5c462749c24
Andy Wong <andywong.ny@gmail.com>
2021-04-19 21:35:58 -0400

    scaffolding for defi dashboard

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fetch = require("node-fetch");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/wallet/:address", (req, res) => {
+  const yieldwatch = "https://www.yieldwatch.net/api/all";
+  const address = req.params.address;
+  const platforms = ["beefy"];
+
+  const url = `${yieldwatch}/${address}?platforms=${platforms.join()}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => res.send(json));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
