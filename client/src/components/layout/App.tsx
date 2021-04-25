347643b1ff9b54c0ddcedc500c42028b1742030c
Andy Wong <andywong.ny@gmail.com>
2021-04-25 17:14:56 -0400

    dashboard draft

--- client/src/components/layout/App.tsx
+++ client/src/components/layout/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout, Menu } from "antd";
 import {
   MenuUnfoldOutlined,
@@ -11,58 +11,60 @@ import "./App.css";
 
 const { Header, Sider, Content } = Layout;
 
-class App extends React.Component {
-  state = {
-    collapsed: false,
-  };
+const App = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const [menu, setMenu] = useState(["dashboard"]);
 
-  toggle = () => {
-    this.setState({
-      collapsed: !this.state.collapsed,
-    });
-  };
-
-  render() {
-    return (
-      <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-            <Menu.Item key="1" icon={<DashboardOutlined />}>
-              Dashboard
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DollarCircleOutlined />}>
-              Payments
-            </Menu.Item>
-            <Menu.Item key="3" icon={<SettingOutlined />}>
-              Settings
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }}>
-            {React.createElement(
-              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-              {
-                className: "trigger",
-                onClick: this.toggle,
-              }
-            )}
-          </Header>
-          <Content
-            className="site-layout-background"
-            style={{
-              margin: "24px 16px",
-              padding: 24,
-              minHeight: 280,
-            }}
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">🦝 Defi Portal</div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={menu}>
+          <Menu.Item
+            key="dashboard"
+            icon={<DashboardOutlined onClick={() => setMenu(["dashboard"])} />}
           >
-            Content
-          </Content>
-        </Layout>
+            Dashboard
+          </Menu.Item>
+          <Menu.Item
+            key="payments"
+            icon={
+              <DollarCircleOutlined onClick={() => setMenu(["payments"])} />
+            }
+          >
+            Payments
+          </Menu.Item>
+          <Menu.Item
+            key="settings"
+            icon={<SettingOutlined onClick={() => setMenu(["settings"])} />}
+          >
+            Settings
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )}
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          Content
+        </Content>
       </Layout>
-    );
-  }
-}
+    </Layout>
+  );
+};
 
 export default App;
